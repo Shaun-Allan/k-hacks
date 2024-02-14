6c7d949bdf8a7d2238d35678a2c683ba0d29fb2a
Shaun-Allan <shaunallan0605@gmail.com>
2024-02-14 15:04:24 +0530

    home page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Nocturnals</h1>
+
+        <div className="buttons">
+          <Button
+            className="button"
+            size="large"
+            onClick={() => {
+              navigate("inputs");
+            }}
+          >
+            Start Simulation
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
